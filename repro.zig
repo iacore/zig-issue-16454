e9e3b0a79e3ac2502533e7a67575f738aabbea54
Locria Cyber <74560659+iacore@users.noreply.github.com>
2023-09-20 19:41:15 +0000

    Solution

---
+++ repro.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+pub fn getExternList(comptime Type: type) []const []const u8 {
+    const Sorter = struct {
+        pub fn lessThan(_: void, lhs: []const u8, rhs: []const u8) bool {
+            return std.ascii.orderIgnoreCase(lhs, rhs) == std.math.Order.lt;
+        }
+    };
+    var extern_list = [_][]const u8{undefined} ** Type.Extern.len;
+    std.mem.copy([]const u8, &extern_list, &Type.Extern);
+    std.sort.block([]const u8, &extern_list, void{}, Sorter.lessThan);
+    return &extern_list;
+}
+test {
+    const t0 = getExternList(struct {
+        pub const Extern = [_][]const u8{ "z", "hey", "hi", "yooo" };
+    })[0];
+    try std.testing.expectEqual(@as([]const u8, "hey"), t0);
+}
